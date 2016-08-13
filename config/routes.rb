db4f06f363f619c20abf6983c2d9ec21019f534f
VineetSahu <shashank.tripathi@mobiloittegroup.com>
2016-08-14 02:41:48 +0530

    callbacks to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'embedded#index'
+  root 'home#index'
 
   get "embedded/signing"
   get "embedded/requesting"
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   post 'oauth', to:'oauth#index'
   get 'oauth', to:'oauth#index'
   
-  resources :embedded do
+  resources :home do
     collection do
       post 'callbacks'
     end
